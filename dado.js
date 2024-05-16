8a844c5323a95e6ab98fdb5daa98d3aef6ae4226
Henrique Douglas <henriquedouglas2803@gmail.com>
2024-05-16 20:30:44 -0300

    corrije comentário

--- dado.js
+++ dado.js
@@ -1,4 +1,4 @@
-const numeroAleatorio = Math.random() * 6 // para gerar um numero aleatorio entre 0 e 9
+const numeroAleatorio = Math.random() * 6 // para gerar um numero aleatorio entre 1 e 6
 const numeroFinal =  Math.ceil(numeroAleatorio);
 console.log(numeroFinal)
 
